e65a7f04b0355adbacb23ee79665afd2641e675f
luanah-souzza <luanah.souzza@hotmail.com>
2024-04-30 20:15:36 -0300

    feat: Adiciona resolução da Questão 3

--- lista-tarefas-1.js
+++ lista-tarefas-1.js
@@ -18,3 +18,19 @@ let percentualValidos = (votosValidos / totalVotos) * 100;
 
 console.log(`A quantidade de votos brancos foi de ${votosBrancos}, de votos nulos ${votosNulos} e de de votos válidos foi de ${votosValidos}`)
 console.log(`O percentuak de votos brancos foi de ${percentualBrancos.toFixed(2)}, de votos nulos ${percentualNulos.toFixed(2)} e de de votos válidos foi de ${percentualValidos.toFixed(2)}`)
+
+// 3. Faça um algoritmo que leia quatro números inteiros e realize as seguintes operações:
+// ● Some 25 ao primeiro inteiro;
+// ● Triplique o valor do segundo inteiro;
+// ● Modifique o valor do terceiro inteiro para 12% do valor original;
+// ● Armazene no quarto inteiro a soma dos valores originais (os que o usuário digitou)
+// dos primeiros três inteiros.
+let num1 = 10;
+let num2 = 6;
+let num3 = 3;
+let num4 = 2;
+console.log(`Resultado final do número 1: ${num1 + 25}`);
+console.log(`Resultado final do número 2: ${num2 * 3}`);
+console.log(`Resultado final do número 3: ${12 / (num3 * 100)}`);
+console.log(`Resultado final do número 4: ${num1 + num2 + num3 + num4}`);
+
