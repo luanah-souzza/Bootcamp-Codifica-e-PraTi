589f707d248569f797486eca9f9c740245ab09a9
luanah-souzza <luanah.souzza@hotmail.com>
2024-04-30 19:48:04 -0300

    feat: Adiciona resolução da Questão 2

--- lista-tarefas-1.js
+++ lista-tarefas-1.js
@@ -3,3 +3,18 @@
 let tempCelcius = 150;
 let tempFah = (tempCelcius * 9/5) + 32;
 console.log(`A temperatura em Fahrenheit é ${tempFah.toFixed(2)}°F`);
+
+// 2. Escreva um algoritmo para ler o número de eleitores de um município, o número de
+// votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa
+// em relação ao total de eleitores.
+let votosBrancos = 50;
+let votosNulos = 50;
+let votosValidos = 50;
+
+let totalVotos = votosBrancos + votosNulos + votosValidos;
+let percentualBrancos = (votosBrancos / totalVotos) * 100;
+let percentualNulos = (votosNulos / totalVotos) * 100;
+let percentualValidos = (votosValidos / totalVotos) * 100;
+
+console.log(`A quantidade de votos brancos foi de ${votosBrancos}, de votos nulos ${votosNulos} e de de votos válidos foi de ${votosValidos}`)
+console.log(`O percentuak de votos brancos foi de ${percentualBrancos.toFixed(2)}, de votos nulos ${percentualNulos.toFixed(2)} e de de votos válidos foi de ${percentualValidos.toFixed(2)}`)
