931a442a352eeed976fd0cda5a213d8ccc784473
luanah-souzza <luanah.souzza@hotmail.com>
2024-04-30 21:22:59 -0300

    feat: Adiciona resolução da Questão 9

--- lista-tarefas-1.js
+++ lista-tarefas-1.js
@@ -94,4 +94,27 @@ if(valor1 > valor2) {
     console.log(`${valor1}, ${valor2}`)
 }
 
+// 9. Escreva um algoritmo que leia o código de origem de um produto e imprima a região
+// do mesmo, conforme a tabela abaixo:
+let codigo = 20;
+if (codigo == 1) {
+    console.log("Sul");
+} else if (codigo == 2) {
+    console.log("Norte");
+} else if (codigo == 3) {
+    console.log("Leste");
+} else if (codigo == 4) {
+    console.log("Oeste");
+} else if (codigo == 5 || codigo == 6) {
+    console.log("Nordeste");
+} else if (codigo >= 7 && codigo <= 9) {
+    console.log("Sudeste");
+} else if (codigo >= 10 && codigo <= 20) {
+    console.log("Centro-Oeste");
+} else if (codigo >= 25 && codigo <= 50) {
+    console.log("Centro-Sul");
+} else {
+    console.log("Fora dos intervalos - Produto importado")
+}
+
 
